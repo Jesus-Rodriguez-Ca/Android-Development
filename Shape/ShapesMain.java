baf0259776dae51ebfd61bd948585f63f19070d3
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-04-20 21:53:54 -0700

    Update ShapesMain.java

--- Shape/ShapesMain.java
+++ Shape/ShapesMain.java
@@ -1,3 +1,10 @@
+// Jesus Rodriguez
+// ITC 155 Spring2020
+
+/*
+	Write a class named Octagon whose objects represts regular octagons. You class should implemet the Shape interface
+	defined in this chapter, including methods for its area and perimeter. An Octagon object is defined by its side lenght.
+*/
 package Shape;
 
 public class ShapesMain {
@@ -5,8 +12,8 @@ public class ShapesMain {
 	public static void main(String[] args) {
 		Shape[] shapes = new Shape[3];
 		shapes[0] = new Octagon(7.5);
-System.out.println("The area of the Octagon is: " + String.format("%.2f", shapes[0].getArea()));
-System.out.println("The perimeter of the Octagon is: " + String.format("%.2f", shapes[0].getPerimeter()));
+	System.out.println("The area of the Octagon is: " + String.format("%.2f", shapes[0].getArea()));
+	System.out.println("The perimeter of the Octagon is: " + String.format("%.2f", shapes[0].getPerimeter()));
 	}
 
 }
