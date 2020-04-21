20e1b1621e985db22fc44f52bf721326c283969e
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-04-20 21:55:58 -0700

    Update Octagon.java

--- Shape/Octagon.java
+++ Shape/Octagon.java
@@ -1,18 +1,26 @@
+// Jesus Rodriguez
+// ITC 155 Spring2020
+
+/*
+	Write a class named Octagon whose objects represts regular octagons. You class should implemet the Shape interface
+	defined in this chapter, including methods for its area and perimeter. An Octagon object is defined by its side lenght.
+*/
+
 package Shape;
 import java.lang.Math;
 
-public class Octagon implements Shape{
+public class Octagon implements Shape{				// Creates an object from the interface Shape
 	private double sideLengh;
 	
-	public Octagon(double sideLengh){
+	public Octagon(double sideLengh){			// Set variable
 		this.sideLengh = sideLengh;	
 	}
 	
-	public double getPerimeter() {
+	public double getPerimeter() {				// Gets perimeter
 		return sideLengh * 8;
 	}
 	
-	public double getArea() {
+	public double getArea() {				// Gets area
 		return 2 * (1 + Math.sqrt(2)) * (sideLengh * sideLengh);
 	}
 }
