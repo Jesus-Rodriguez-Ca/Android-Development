0b13fac7f19e3132eb0e80e4ef0a2ec178ca32e6
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-04-28 19:52:15 -0700

    Update ArrayListMain.java

--- arrayList/ArrayListMain.java
+++ arrayList/ArrayListMain.java
@@ -1,10 +1,24 @@
-package arrayList;
+// Jesus Rodriguez
+// 4-28-2020
+// ITC 155 Spring 2020
+/*	Write a method called removeEvenLength that takes an ArrayList of strings as a parameter 
+ * 	and removes all of the strings of even length from the list. */
 
-public class ArrayListMain {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void removeEvenLength(ArrayList<String> even) {  // Method accepts an ArrayList as parameter
+			
+		for (int i = 0; i < even.size(); i++) {		// Loop starts in 0 ends according length increases in 1
+		    String word = even.get(i);			// Variable "word" gets the value of the ArrayList in every iteration 
+		     if (word.length() % 2 == 0) { // If the remainder between the length of the value on word and 2 is equal to 0
+		        even.remove(i);				//then every strings that are even will be remove.
+		            i--;
+		        }
+		    }
+		}
 
-	}
 
-}
+		    
+		
+
