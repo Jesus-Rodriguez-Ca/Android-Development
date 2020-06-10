05788c4f373451d9d22e225d43f9749d52a7c798
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-06-09 22:04:53 -0700

    Add files via upload

---
+++ linked/List.java
@@ -0,0 +1,37 @@
+// Jesus Rodriguez
+// ITC 155 Data Structure
+// Spring 2020
+// 6-8-2020
+
+
+/* Write a method isSorted that returns true if the list is in sorted 
+*  (nondecreasing) order and returns false otherwise. An empty list is 
+*  considered to be sorted. 
+*/
+
+package linked;
+
+public class List {
+	
+	private static ListNode front;
+
+	public static boolean isSorted() {
+	    
+		
+		ListNode pre = front;
+	    if (front == null) {
+	        return true;
+	    }
+	    
+	    while (pre.next != null) {
+	        ListNode current = pre.next;
+	        if (current.data < pre.data) {
+	            return false;
+	        }
+	        pre = current;
+	    }
+	    
+	    return true;
+	}
+
+}
